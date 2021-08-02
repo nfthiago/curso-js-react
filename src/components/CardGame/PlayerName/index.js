f0060c25ee1b2f87742dc74e1c9f08005c46c940
nfthiago <nfthiago@gmail.com>
2021-08-02 10:59:54 +0100

    :sparkles: Add CSS of the PlayerName component

--- src/components/CardGame/PlayerName/index.js
+++ src/components/CardGame/PlayerName/index.js
@@ -1,9 +1,9 @@
-
+import "./style.css"
 
 function PlayerName(content) {
     return `
-    <p class="player-num">
-    <img src="images/${content}.png" alt="Logo da Alura">
+    <p class="player-name">
+    ${content}
     </p>
     `
 }
